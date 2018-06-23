f67f0e0122a04cb43d8b6bd164214753b81baf54
Emilian Roman <roman.emilian@outlook.com>
2018-06-23 10:35:12 +0800

    Develop the Main model and its VM

---
+++ Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace YuMi.NieRexper
+{
+    public class Main : ILevelApply
+    {
+        int Address {
+            get { return 0x0003871C; }
+        }
+
+        public static UInt32 Level10 {
+            get { return 0x00C70; }
+        }
+
+        public static UInt32 Level25 {
+            get { return 0x086BD; }
+        }
+
+        public static UInt32 Level50 {
+            get { return 0x330E7; }
+        }
+
+        public static UInt32 Level75 {
+            get { return 0x92847; }
+        }
+
+        public LevelApplyResult Apply(UInt32 amount, String path)
+        {
+            try
+            {
+                using (var writer = new BinaryWriter(File.OpenWrite(path)))
+                {
+                    var value = BitConverter.GetBytes(amount);
+                    writer.BaseStream.Seek(Address, SeekOrigin.Begin);
+                    writer.Write(value, 0, value.Length);
+
+                    return new LevelApplyResult(LevelApplyStatus.Success);
+                }
+            }
+            catch (Exception e)
+            {
+                return new LevelApplyResult(LevelApplyStatus.Exception, e.Message);
+            }
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+
+namespace YuMi.NieRexper
+{
+    public class MainViewModel
+    {
+        Main main = new Main();
+
+        string SavesDirectory {
+            get {
+                var myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                return Path.Combine(myDocs, "My Games", "NieR_Automata");
+            }
+        }
+
+        public void ApplyEXP(UInt32 amount)
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                InitialDirectory = SavesDirectory,
+                Filter = "N:A Saves (SlotData_*.dat)|SlotData_*.dat"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                main.Apply(amount, openFileDialog.FileName);
+            }
+        }
+    }
+}
