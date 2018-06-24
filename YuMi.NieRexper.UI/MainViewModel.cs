4a5c17c61db92db9d89a085c9b0842fd26cbdf51
Emilian Roman <roman.emilian@outlook.com>
2018-06-24 14:21:12 +0800

    Create properties for representing slot-related states

--- YuMi.NieRexper.UI/MainViewModel.cs
+++ YuMi.NieRexper.UI/MainViewModel.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using YuMi.NieRexper.Apply.Common;
 
 namespace YuMi.NieRexper.UI
@@ -13,10 +13,16 @@ namespace YuMi.NieRexper.UI
 
         string statusText = Properties.Resources.StatusAwaiting;
 
+        string slotFile = string.Empty;
+
+        Visibility slotsGridVisibility = Visibility.Visible;
+
+        Visibility levelsGridVisibility = Visibility.Collapsed;
+
         /// <summary>
         /// Full path for the NieR:Automata saves directory: Documents\My Games\NieR_Automata
         /// </summary>
-        string SavesDirectory {
+        string SlotsDirectory {
             get {
                 var myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 return Path.Combine(myDocs, Properties.Resources.GamesDirectory, Properties.Resources.SavesDirectory);
@@ -40,25 +46,103 @@ namespace YuMi.NieRexper.UI
         }
 
         /// <summary>
+        /// Absolute path representation of the selected slot.
+        /// </summary>
+        public string SlotFile {
+            get {
+                return slotFile;
+            }
+            set {
+                if (value != slotFile)
+                {
+                    slotFile = value;
+                    NotifyPropertyChanged();
+                    SlotsGridVisibility = Visibility.Collapsed;
+                    LevelsGridVisibility = Visibility.Visible;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Toggles whether the Slot 0 selection button is enabled or not depending on Slot 0's existence.
+        /// </summary>
+        public bool Slot0ButtonEnabled {
+            get {
+                return SlotExists(Properties.Resources.SlotData0);
+            }
+        }
+
+        /// <summary>
+        /// Toggles whether the Slot 0 selection button is enabled or not depending on Slot 1's existence.
+        /// </summary>
+        public bool Slot1ButtonEnabled {
+            get {
+                return SlotExists(Properties.Resources.SlotData1);
+            }
+        }
+
+        /// <summary>
+        /// Toggles whether the Slot 0 selection button is enabled or not depending on Slot 2's existence.
+        /// </summary>
+        public bool Slot2ButtonEnabled {
+            get {
+                return SlotExists(Properties.Resources.SlotData2);
+            }
+        }
+
+        /// <summary>
+        /// Toggle visibility based on whether a slot has been selected or not.
+        /// </summary>
+        public Visibility SlotsGridVisibility {
+            get {
+                return slotsGridVisibility;
+            }
+            set {
+                if (slotsGridVisibility != value)
+                {
+                    slotsGridVisibility = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Toggle visibility based on whether a slot has been selected or not.
+        /// </summary>
+        public Visibility LevelsGridVisibility {
+            get {
+                return levelsGridVisibility;
+            }
+            set {
+                if (levelsGridVisibility != value)
+                {
+                    levelsGridVisibility = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
         /// Prompt the user with a dialogue window to choose their save slot and apply the specified EXP value.
         /// </summary>
         /// <param name="amount">Amount of EXP to apply to the save slot the user will choose.</param>
         public void ApplyEXP(int amount)
         {
-            var openFileDialog = new OpenFileDialog()
-            {
-                InitialDirectory = SavesDirectory,
-                Filter = Properties.Resources.SavesFilter
-            };
+            var result = main.PatchSlot(Path.Combine(SlotsDirectory, SlotFile), amount);
 
-            if (openFileDialog.ShowDialog() == true)
-            {
-                var result = main.PatchSlot(openFileDialog.FileName, amount);
+            StatusText = result.Status == PatchStatus.Success
+                ? Properties.Resources.StatusSuccess
+                : result.Data;
+        }
 
-                StatusText = result.Status == PatchStatus.Success
-                    ? Properties.Resources.StatusSuccess
-                    : result.Data;
-            }
+        /// <summary>
+        /// Checks the NieR:Automata saves (slots) directory for the existence of the specified slot file.
+        /// </summary>
+        /// <param name="slot">Slot file to check, e.g. SlotData_0.dat</param>
+        /// <returns>Provided slot file exists on the filesystem.</returns>
+        bool SlotExists(string slot)
+        {
+            return File.Exists(Path.Combine(SlotsDirectory, slot));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
