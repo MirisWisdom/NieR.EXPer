53f7e894fe62ced6204b957af48e1285f0eee7e5
Emilian Roman <roman.emilian@outlook.com>
2018-06-24 19:03:13 +0800

    Add interface and type for determining required EXP for a given level

---
+++ YuMi.NieRexper/Calculate/ICalculate.cs
@@ -0,0 +1,15 @@
+﻿namespace YuMi.NieRexper.Calculate
+{
+    /// <summary>
+    /// Interface to be implemented by types which deal with calculating return values based on the inbound values.
+    /// </summary>
+    public interface ICalculate
+    {
+        /// <summary>
+        /// Calculate the return value based on the given value.
+        /// </summary>
+        /// <param name="value">Value to used for the calculation.</param>
+        /// <returns>Calculated value.</returns>
+        int Calculate(int value);
+    }
+}
