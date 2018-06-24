cdacfe840d812be929d0b38a1243cc1ae44ccdf1
Emilian Roman <roman.emilian@outlook.com>
2018-06-24 13:22:42 +0800

    Implement the slot patching logic into the new library

---
+++ YuMi.NieRexper/Patch/Common/PatchStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace YuMi.NieRexper.Apply.Common
+{
+    public enum PatchStatus
+    {
+        /// <summary>
+        /// The amount of EXP required for the chosen level has been successfully applied.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// An exception has occurred when applying the EXP.
+        /// </summary>
+        Exception
+    }
+}
---
+++ YuMi.NieRexper/Patch/IPatch.cs
@@ -0,0 +1,14 @@
+﻿using YuMi.NieRexper.Apply.Common;
+
+namespace YuMi.NieRexper.Apply
+{
+    public interface IPatch
+    {
+        /// <summary>
+        /// Patch experience points to a resource.
+        /// </summary>
+        /// <param name="amount">Amount of EXP to patch to the resource.</param>
+        /// <returns>Result object representing the outcome of the patch procedure.</returns>
+        PatchResult Patch(int amount);
+    }
+}
