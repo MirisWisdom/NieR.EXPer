ca6e15d2997213bd5530463a8f364e8d2faebcee
Emilian Roman <roman.emilian@outlook.com>
2019-01-05 10:32:21 +0800

    Introduce CLI for patching a save slot with a given level

---
+++ src/YuMi.NieRexper.CLI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace YuMi.NieRexper.CLI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Not enough args.");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                Task.Run(() =>
+                {
+                    var levelExp = ExperienceFactory.FromLevel((Level) int.Parse(args[0]));
+                    Console.WriteLine($"Infer points value: [{levelExp.Points}] <= [{args[0]}]");
+
+                    var saveSloth = (Slot) args[1];
+                    Console.WriteLine($"Patching save slot: [{saveSloth.Path}] <= [{levelExp.Points}]");
+
+                    new ExperienceRepository(saveSloth).Save(levelExp);
+                    Console.WriteLine($"Successfully saved: [{args[1]}] <= [{args[0]}]");
+                }).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                Environment.Exit(1);
+            }
+        }
+    }
+}
