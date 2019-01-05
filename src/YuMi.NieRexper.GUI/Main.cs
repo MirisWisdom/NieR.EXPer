e259809d72b577c23e181346abb8e40f714c4f6e
Emilian Roman <roman.emilian@outlook.com>
2019-01-05 16:25:19 +0800

    Redevelop the UI layout & logic

---
+++ src/YuMi.NieRexper.GUI/Main.cs
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C) 2018-2019 Emilian Roman
+ * 
+ * This file is part of NieR.EXPer.
+ * 
+ * NieR.EXPer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NieR.EXPer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NieR.EXPer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using YuMi.NieRexper.GUI.Annotations;
+
+namespace YuMi.NieRexper.GUI
+{
+    /// <summary>
+    ///     Main model for the GUI.
+    /// </summary>
+    public class Main : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///     <see cref="ExpLevel" />
+        /// </summary>
+        private int _expLevel;
+
+        /// <summary>
+        ///     <see cref="SaveSlot" />
+        /// </summary>
+        private int _saveSlot;
+
+        /// <summary>
+        ///     <see cref="Status" />
+        /// </summary>
+        private string _status = "PENDING";
+
+        /// <summary>
+        ///     Selected save slot.
+        /// </summary>
+        public int SaveSlot
+        {
+            get => _saveSlot;
+            set
+            {
+                if (value == _saveSlot) return;
+                _saveSlot = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Wanted level.
+        /// </summary>
+        public int ExpLevel
+        {
+            get => _expLevel;
+            set
+            {
+                if (value == _expLevel) return;
+                _expLevel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Current program status.
+        /// </summary>
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                if (value == _status) return;
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Invokes the library's EXP calculation & slot patching components.
+        /// </summary>
+        public void SaveData()
+        {
+            try
+            {
+                var personal = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var slotPath = Path.Combine(personal, "My Games", "NieR_Automata", $"SlotData_{SaveSlot}.dat");
+
+                var levelExp = ExperienceFactory.FromLevel((Level) ExpLevel);
+                var saveSloth = (Slot) slotPath;
+
+                new ExperienceRepository(saveSloth).Save(levelExp);
+
+                Status = "SUCCESS";
+            }
+            catch (Exception exception)
+            {
+                Status = exception.Message;
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/YuMi.NieRexper.GUI/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿/**
+ * Copyright (C) 2018-2019 Emilian Roman
+ * 
+ * This file is part of NieR.EXPer.
+ * 
+ * NieR.EXPer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NieR.EXPer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NieR.EXPer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace YuMi.NieRexper.GUI
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private readonly Main _main;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _main = (Main) DataContext;
+
+            MainTabControl.SelectedItem = SaveSlotTabItem;
+        }
+
+        private void SetSaveSlot00(object sender, RoutedEventArgs e)
+        {
+            SetSaveSaveSlot(0);
+        }
+
+        private void SetSaveSlot01(object sender, RoutedEventArgs e)
+        {
+            SetSaveSaveSlot(1);
+        }
+
+        private void SetSaveSlot02(object sender, RoutedEventArgs e)
+        {
+            SetSaveSaveSlot(2);
+        }
+
+        private void SetExpLevel10(object sender, RoutedEventArgs e)
+        {
+            SetExpLevel(10);
+        }
+
+        private void SetExpLevel25(object sender, RoutedEventArgs e)
+        {
+            SetExpLevel(25);
+        }
+
+        private void SetExpLevel50(object sender, RoutedEventArgs e)
+        {
+            SetExpLevel(50);
+        }
+
+        private void SetExpLevel75(object sender, RoutedEventArgs e)
+        {
+            SetExpLevel(75);
+        }
+
+        private void SetSaveSaveSlot(int slot)
+        {
+            _main.SaveSlot = slot;
+            MainTabControl.SelectedItem = ExpLevelTabItem;
+        }
+
+        private void SetExpLevel(int level)
+        {
+            _main.ExpLevel = level;
+            _main.SaveData();
+        }
+
+        private void SetCustomExpLevel(object sender, RoutedEventArgs e)
+        {
+            _main.SaveData();
+        }
+    }
+}
